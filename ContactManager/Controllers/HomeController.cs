d08fa1246527cd8ad54b53f25cd9822406e0df99
TDeMaria64 <tdemaria64@gmail.com>
2022-08-26 00:10:48 -0400

    Add project files.

---
+++ ContactManager/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
+using System.Threading.Tasks;
+
+namespace ContactManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ContactContext Context { get; set; }
+        public HomeController(ContactContext ctx)
+        {
+            Context = ctx;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var contacts = from c in Context.Contacts
+                           .Include(c => c.Category)
+                           .OrderBy(c => c.Lastname)
+                           select c;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                contacts = contacts.Where(s => s.Lastname!.Contains(searchString));
+            }
+
+            return View(await contacts.ToListAsync());
+        }
+    }
+}
